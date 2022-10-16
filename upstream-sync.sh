0106a176531db2d2a33f30ee3b2110be2a0ebad8
shreyapohekar <shreyapohekar@gmail.com>
2022-10-16 11:40:08 +0530

    Update upstream-sync.sh

--- upstream-sync.sh
+++ upstream-sync.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt install git
+
 set -e
 
 # fail if upstream_repository is not set in workflow
@@ -39,10 +43,10 @@ git log upstream/"${INPUT_TARGET_BRANCH}" "${LOCAL_COMMIT_HASH}"..HEAD --pretty=
 
 # pull from upstream to target_branch
 echo 'Pulling...' 1>&1
-git pull upstream "${INPUT_UPSTREAM_BRANCH}"
+git pull upstream "${INPUT_UPSTREAM_BRANCH}" --allow-unrelated-histories
 echo 'Pull successful' 1>&1
 
 # push to origin target_branch
 echo 'Pushing to target branch...' 1>&1
-git push origin "${INPUT_TARGET_BRANCH}"
+git push origin "${INPUT_TARGET_BRANCH}" --allow-unrelated-histories
 echo 'Push successful' 1>&1
